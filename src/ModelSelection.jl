ce22ea80d335ffd37f4c0e9767f65745d1ea718f
Adán Mauri Ungaro <adan.mauri@gmail.com>
2023-03-22 00:38:33 -0300

    Fixing AllSubsetRegression error

--- src/ModelSelection.jl
+++ src/ModelSelection.jl
@@ -7,13 +7,14 @@ include("datatypes/modelselection_result.jl")
 include("strings.jl")
 include("const.jl")
 include("utils.jl")
-include("core.jl")
 
 include("Preprocessing/Preprocessing.jl")
 include("FeatureExtraction/FeatureExtraction.jl")
 include("PreliminarySelection/PreliminarySelection.jl")
 include("AllSubsetRegression/AllSubsetRegression.jl")
 include("CrossValidation/CrossValidation.jl")
+
+include("core.jl")
 
 using ..Preprocessing
 using ..FeatureExtraction
