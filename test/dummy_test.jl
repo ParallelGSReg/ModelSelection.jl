9871b101d00be66e81c3400921978f8d012a741a
dpanigo <panigo@gmail.com>
2023-03-26 11:06:35 -0300

    minor changes in test

--- test/dummy_test.jl
+++ test/dummy_test.jl
@@ -1,6 +1,14 @@
-using CSV, ModelSelection, DataFrames
+using CSV, ModelSelection, DataFrames, Distributions
 
 data = CSV.read("test/data/visitors.csv", DataFrame)
+p=0.3
+N=size(data,1)
+d=Binomial(1,p)
+data[!,:y]=rand(d,N)
 
-model =
-    ModelSelection.gsr(:logit, "australia china japan uk", data, criteria = [:aic, :aicc])
+
+model =  ModelSelection.gsr(:logit, "y australia china japan uk", data, criteria = [:aic, :aicc])
+
+ModelSelection.save_csv("result.csv", model)
+ModelSelection.save("result.jld", model) 
+results = ModelSelection.load("result.jld")
