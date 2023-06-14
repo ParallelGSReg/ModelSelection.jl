3fffdea8ed51302a93ea9d913a2dbc35e4c9e825
Adán Mauri Ungaro <adan.mauri@gmail.com>
2023-06-14 19:12:40 -0300

    Actions improvements

--- docs/make.jl
+++ docs/make.jl
@@ -4,14 +4,15 @@ using Documenter, DocumenterTools
 using ModelSelection, ModelSelection.AllSubsetRegression
 
 makedocs(
-    #format = Documenter.HTML(),
     source = "src",
     build   = "build",
     clean   = true,
     modules = [ModelSelection, ModelSelection.AllSubsetRegression],
     sitename = "Modelselection.jl",
     # expandfirst
-    pages = ["Home" => "index.md"],
+    pages = [
+        "Home" => "index.md",
+    ],
 )
 
 deploydocs(repo = REPO)
