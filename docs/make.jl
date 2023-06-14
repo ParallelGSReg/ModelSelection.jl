f572166482ecc40eafd2cd182d39b2b3e23c4ff0
Adán Mauri Ungaro <adan.mauri@gmail.com>
2023-06-14 13:13:03 -0300

    Documentation improvements

--- docs/make.jl
+++ docs/make.jl
@@ -1,28 +1,17 @@
-using Pkg
-Pkg.activate(".")
+const REPO = "https://github.com/ParallelGSReg/ModelSelection.jl.git"
+
+using Documenter, DocumenterTools
 using ModelSelection, ModelSelection.AllSubsetRegression
 
-using Distributions, Documenter
-
 makedocs(
-    #root = "/",
     #format = Documenter.HTML(),
-    sitename = "Modelselection.jl",
-    modules = [ModelSelection, ModelSelection.AllSubsetRegression],
-    repo = "https://github.com/ParallelGSReg/ModelSelection.j",
     source = "src",
-    doctest = true,
-    clean   = true,
     build   = "build",
-    debug = true,
-    
+    clean   = true,
+    modules = [ModelSelection, ModelSelection.AllSubsetRegression],
+    sitename = "Modelselection.jl",
+    # expandfirst
     pages = ["Home" => "index.md"],
 )
 
-#deploydocs(repo = "github.com/ParallelGSReg/ModelSelection.jl.git")
-
-#makedocs(...,
-#    format = Documenter.HTML(
-#        prettyurls = get(ENV, "CI", nothing) == "true"
-#    )
-#)
+deploydocs(repo = REPO)
