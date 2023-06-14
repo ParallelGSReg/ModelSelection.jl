7ef5dfdb33bc5b073e3b7dd1814932dfec44231d
Documenter.jl <documenter@juliadocs.github.io>
2023-06-14 19:15:19 +0000

    build based on 3fffdea

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#ModelSelection","page":"Home","title":"ModelSelection","text":"","category":"section"},{"location":"#Credits","page":"Home","title":"Credits","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"The GSReg module, which perform regression analysis, was written primarily by Demian Panigo, Adán Mauri Ungaro, Nicolás Monzón and Valentín Mari. The ModelSelection.jl module was inpired by GSReg for Stata, written by Pablo Gluzmann and Demian Panigo.","category":"page"}]
+}
