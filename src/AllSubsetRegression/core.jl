014ef1bbf74ee1fd9452b73a3a7cdc537eed9684
Demian Panigo <panigo@gmail.com>
2023-07-20 09:06:22 -0300

    summary_variables fixed

--- src/AllSubsetRegression/core.jl
+++ src/AllSubsetRegression/core.jl
@@ -121,7 +121,7 @@ function to_string(data::ModelSelectionData, result::AllSubsetRegressionResult)
     datanames_index = ModelSelection.create_datanames_index(result.datanames)
 
     #MODIFICAMOS SUMMARY VARIABLES SEGUN ESTIMADOR
-    summary_variables = (result.estimator = :logit ? SUMMARY_VARIABLES_LOGIT : SUMMARY_VARIABLES_OLS)
+    summary_variables = (data.options[:estimator] = :logit ? SUMMARY_VARIABLES_LOGIT : SUMMARY_VARIABLES_OLS)
     if :r2adj in result.datanames
         summary_variables[:r2adj] = Dict("verbose_title" => "Adjusted R²", "verbose_show" => true)  # FIXME: Use the dictionary
     end
@@ -205,7 +205,7 @@ result_string = to_dict(model_selection_data, all_subset_regression_result)
 """
 function to_dict(data::ModelSelectionData, result::AllSubsetRegressionResult)
     #summary variables depend on estimator
-    summary_variables = copy((result.estimator = :logit ? SUMMARY_VARIABLES_LOGIT : SUMMARY_VARIABLES_OLS))
+    summary_variables = copy((data.options[:estimator] = :logit ? SUMMARY_VARIABLES_LOGIT : SUMMARY_VARIABLES_OLS))
     datanames_index = ModelSelection.create_datanames_index(result.datanames)
     best_results_expvars = ModelSelection.get_selected_variables_varnames(
         Int64(result.bestresult_data[datanames_index[:index]]),
